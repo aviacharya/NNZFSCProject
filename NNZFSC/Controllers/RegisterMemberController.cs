b6be224051fa098f118410f324c1e8d7d999218d
Avishek <aviacharya2002@yahoo.com>
2018-06-22 11:26:58 +1200

    Changing the UI layout to dashboard compatible
    customizing the dashboard template

--- NNZFSC/Controllers/RegisterMemberController.cs
+++ NNZFSC/Controllers/RegisterMemberController.cs
@@ -49,8 +49,8 @@ namespace NNZFSC.Controllers
         public ActionResult Details( int id)
         {
             MemberRegistration MemberRegistration = new MemberRegistration();
-            var memberDetails = objRegisterMember.GetMemberById(0);
-            var paymentDetails = objPaymentMember.GetPaymentDetails(0);
+            var memberDetails = objRegisterMember.GetMemberById(3);
+            var paymentDetails = objPaymentMember.GetPaymentDetails(3);
             if (paymentDetails.Count() == 0 && memberDetails.MemberId == 0)
             {
                 return View(memberDetails);
